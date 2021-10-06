b865320f8191770c14613da582c2b9db32499822
r2dev2 <ronak.badhe@gmail.com>
2021-10-05 23:42:02 -0700

    can get the html for each unit

--- scripts/bale.py
+++ scripts/bale.py
@@ -1,3 +1,4 @@
+import re
 from base64 import b64encode
 from pathlib import Path
 
@@ -5,7 +6,7 @@ import requests
 from beartype import beartype
 from bs4 import BeautifulSoup
 
-from models import EconData
+from models import EconData, ImageLink, Unit
 
 
 __cache_root = Path(__file__).parent / "../balesite"
@@ -17,15 +18,51 @@ __welcome_selector = "#comp-jyz8knnk"
 @beartype
 def get_econ_main_data(cached: bool=True) -> EconData:
     html = __get_site(__economics_url)
-    welcome = __get_main_welcome(html)
-    return EconData(welcome=welcome, units=[])
+    return EconData(welcome=__get_main_welcome(html), units=__get_main_units(html))
 
 
 @beartype
 def __get_main_welcome(html: str) -> str:
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, "html.parser")
     for welcome_el in soup.select(__welcome_selector):
         return __correct_text(welcome_el.text)
+
+
+@beartype
+def __get_main_units(html: str) -> list[Unit]:
+    htmls = __get_unit_htmls(html)
+    for html in htmls:
+        print(BeautifulSoup(html, "html.parser").text)
+        print("\n\n")
+    return []
+
+
+@beartype
+def __get_unit_htmls(html: str) -> list[str]:
+    # FIXME extract to function
+    htmls = []
+    start = html.find("<h2")
+    end = html.find("</h2", start + 1)
+    while -1 not in {start, end}:
+        htmls.append(html[start:end])
+        start = html.find("<h2", end + 1)
+        end = html.find("</h2", start + 1)
+
+    # FIXME extract to function
+    results = []
+    buf = []
+    for bloc in htmls:
+        # FIXME this nesting is ugly jesus
+        # FIXME do a better check for unit when you stop being lazy
+        if "Unit" in bloc:
+            if buf:
+                results.append("\n".join(buf))
+            buf = [bloc]
+            continue
+        if "Unit" in "".join(buf[:1]):
+            buf.append(bloc)
+
+    return [*results, "\n".join(buf)]
 
 
 @beartype
