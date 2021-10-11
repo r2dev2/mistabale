dafa12d11fcea82e070472c14e74b1661e37ab16
r2dev2 <ronak.badhe@gmail.com>
2021-10-10 23:07:55 -0700

    better parsing for data at end of each unit

--- scripts/bale.py
+++ scripts/bale.py
@@ -17,7 +17,7 @@ __cache_root.mkdir(exist_ok=True)
 __economics_url = "https://www.mistabale.com/economics"
 __welcome_selector = "#comp-jyz8knnk"
 __img_link_selector = "a[data-testid=linkElement]"
-__text_link_selector = "p > a"
+__text_link_selector = "p a"
 __unit_divider = re.compile(r"Unit \d")
 __width_finder = re.compile(r"w_(\d+)")
 __height_finder = re.compile(r"h_(\d+)")
@@ -71,16 +71,13 @@ def __get_mainpage_bloc_idxs(html: str) -> Iterable[tuple[int, int]]:
 def __separate_into_units(
     html: str, bloc_idxs: Iterable[tuple[int, int]]
 ) -> Iterable[str]:
-    unit_start, unit_end = [-1] * 2
+    unit_start = -1
     for start, end in bloc_idxs:
         if re.search(__unit_divider, html[start:end]):
-            if -1 not in [unit_start, unit_end]:
-                yield html[unit_start:unit_end]
-            unit_start, unit_end = start, end
-            continue
-        if -1 not in [unit_start, unit_end]:
-            unit_end = end
-    yield html[unit_start:unit_end]
+            if unit_start != -1:
+                yield html[unit_start:start]
+            unit_start = start
+    yield html[unit_start:end]
 
 
 @beartype
