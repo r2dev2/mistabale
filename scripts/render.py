0fd6dee451edcf40aff7699975f98b1e8525c94a
r2dev2 <ronak.badhe@gmail.com>
2021-10-15 19:49:49 -0700

    prefetching and link to quarterly report + case studies

--- scripts/render.py
+++ scripts/render.py
@@ -1,6 +1,9 @@
 from beartype import beartype
 from models import EconData, ImageLink, TextLink, Unit
 
+__quartery_reports = "https://www.mistabale.com/quarterly-reports"
+__case_studies = "https://www.mistabale.com/case-studies"
+
 
 @beartype
 def render_econ_data(data: EconData) -> str:
@@ -12,13 +15,25 @@ def render_econ_data(data: EconData) -> str:
     <meta charset="UTF-8">
     <title>Economics - Mistabale</title>
     <link rel="stylesheet" href="./index.css" />
+    <link rel="prefetch" href="{__quartery_reports}" />
+    <link rel="prefetch" href="{__case_studies}" />
+    {"".join(f'<link rel="prefetch" href="./Unit-{i}.html" />' for i in range(1, len(data.units) + 1))}
 </head>
 <body>
     <h1>Economics - Mistabale</h1>
-    <h2>Units:</h2>
-    <ul>
-    {"".join(f'<li><a href="./Unit-{i}.html">{unit.title}</a></li>' for i, unit in enumerate(data.units, 1))}
-    </ul>
+    <section>
+        <h2>General</h2>
+        <ul>
+            <li><a href="{__quartery_reports}">Quarterly reports</a></li>
+            <li><a href="{__case_studies}">Case studies</a></li>
+        </ul>
+    </section>
+    <section>
+        <h2>Units</h2>
+        <ul>
+        {"".join(f'<li><a href="./Unit-{i}.html">{unit.title}</a></li>' for i, unit in enumerate(data.units, 1))}
+        </ul>
+    </section>
 </body>
 </html>
 """
