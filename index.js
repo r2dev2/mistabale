0812e1fee3921fb05493ff2c55117cfdfde05960
r2dev2 <50760816+r2dev2@users.noreply.github.com>
2021-10-23 18:24:21 +0000

    Deploying to gh-pages from  @ eafe23793df8d48d85d65d5abda309c492a79e60 🚀

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+let clientX = null;
+let clientY = null;
+
 const cursor = document.querySelector('.cursor');
-window.addEventListener('mousemove', e => {
-  const x = e.clientX - 15;
-  const y = e.clientY - 17;
+const focussed = 'a.focussed,a:hover,.resource.focussed';
+
+const updateCursor = (clientX, clientY, scrollX, scrollY) => {
+  if ([clientX, clientY].includes(null)) return;
+  const x = clientX - 15 + scrollX;
+  const y = clientY - 17 + scrollY;
   cursor.style = `transform: translate(${x}px, ${y}px)`;
   cursor.classList.add('active');
   cursor.classList[
-    document.querySelectorAll('a.focussed,a:hover').length ? 'add' : 'remove'
+    document.querySelectorAll(focussed).length ? 'add' : 'remove'
   ]('focussed');
-});
+};
+
+window.addEventListener('mousemove', e => updateCursor(
+  clientX = e.clientX, clientY = e.clientY, window.scrollX, window.scrollY
+));
+
+window.addEventListener('scroll', () => updateCursor(
+  clientX, clientY, window.scrollX, window.scrollY
+));
 
 const on = (el, event, success, failure) => window.addEventListener(event, e => {
   const { x, y, width, height } = el.getBoundingClientRect();
   const inBetween = (start, d, num) => start <= num && num <= start + d;
-  if (inBetween(x, width, e.clientX) && inBetween(y, height, e.clientY)) {
+  if (inBetween(x, width, e.clientX) && inBetween(y, height, e.clientY))
+  {
     e.preventDefault();
     return success(el);
   }
@@ -21,8 +36,8 @@ const on = (el, event, success, failure) => window.addEventListener(event, e => 
 
 const addFocussed = el => el.classList.add('focussed');
 const rmFocussed = el => el.classList.remove('focussed');
-const navigateTo = a => window.location = a;
-document.querySelectorAll('a').forEach(a => {
+const navigateTo = a => window.location.href = a?.href ?? a.querySelector('a')?.href;
+document.querySelectorAll('a,.resource').forEach(a => {
   on(a, 'mousemove', addFocussed, rmFocussed);
   on(a, 'contextmenu', () => { }, () => { });
   on(a, 'click', navigateTo, () => { });
